835b393fe410a2a7076c768df4f8daf5d9f2cfbf
William Verde <williamverde10@gmail.com>
2019-02-26 18:32:39 -0400

    files lang

---
+++ Resources/lang/es/icommercefreeshippings.php
@@ -0,0 +1,31 @@
+<?php
+
+return [
+    'single' => 'Freeshipping',
+    'description' => 'La descripcion del Modulo',
+    'list resource' => 'List icommercefreeshippings',
+    'create resource' => 'Create icommercefreeshippings',
+    'edit resource' => 'Edit icommercefreeshippings',
+    'destroy resource' => 'Destroy icommercefreeshippings',
+    'title' => [
+        'icommercefreeshippings' => 'IcommerceFreeshipping',
+        'create icommercefreeshipping' => 'Create a icommercefreeshipping',
+        'edit icommercefreeshipping' => 'Edit a icommercefreeshipping',
+    ],
+    'button' => [
+        'create icommercefreeshipping' => 'Create a icommercefreeshipping',
+    ],
+    'table' => [
+        'minimum' => 'Minimum total of the order (Products without Freeshipping)',
+    ],
+    'form' => [
+    ],
+    'messages' => [
+        'msjini' => 'Debes ingresar el pais',
+        'withoutfree' => 'No se ha asignado pais para freeshipping',
+        'notavailable' => 'No disponible para ese destino',
+        'totalmininum' => 'El total minimo de la orden (productos sin freeshipping) debe ser:' 
+    ],
+    'validation' => [
+    ],
+];
